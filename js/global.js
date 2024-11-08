77f1aff699d7442384a4b08de9719a1d54cb64b8
lmace-eriks <lucasmace4130@gmail.com>
2024-11-07 21:30:44 -0600

    Componentry, Anchor Inturrupiton, skills, resume pages.

--- js/global.js
+++ js/global.js
@@ -1,3 +1,26 @@
+const dom = new Map();
+
+const addElementsToDomMap = (parentElement = document.body) => {
+  const allIdElements = parentElement.querySelectorAll(`[id]`);
+  for (const element of allIdElements) dom.set(element.id, element);
+};
+
+addElementsToDomMap();
+
+// Limits the frequency of function calls to 4 times
+// per second rather than every mutation event.
+let domThrottle = null;
+const runDomMapThrottle = () => {
+  if (domThrottle !== null) return;
+
+  domThrottle = setTimeout(() => {
+    addElementsToDomMap();
+    assignAnchorListeners();
+
+    domThrottle = null;
+  }, 250);
+};
+
 const handleHamburgerClick = (event) => {
   const target = event.currentTarget || event.target;
   const navIsOpen = target.getAttribute("aria-expanded") === "true";
@@ -23,29 +46,53 @@ const adjustTabIndexOfDrawerItems = (navIsOpen) => {
   for (const element of allFocusableElements) element.setAttribute("tabindex", navIsOpen ? -1 : 0);
 };
 
-// Don't keep this. Just having a goof.
-const ultimateNonBreakingSpaceInsert = () => {
-  const allParagraphs = document.querySelectorAll("p");
+// Only needs to run once on initial page load.
+const importComponents = async () => {
+  const requestHeader = await fetch("./components/header.html");
+  const requestHeaderText = await requestHeader.text();
 
-  for (const p of allParagraphs) {
-    const wordList = p.innerText.split(" ");
+  const siteHeaderId = "site-header";
+  dom.get(siteHeaderId).innerHTML = requestHeaderText;
 
-    let pBuilder = "";
+  // Add newly imported components to dom map.
+  addElementsToDomMap(dom.get(siteHeaderId));
 
-    wordList.forEach((word, index) => {
-      pBuilder = pBuilder + `${index !== wordList.length - 1 ? " " : "&nbsp;"}` + word;
-    });
-
-    p.innerHTML = pBuilder;
-  }
+  // Once added to dom map, set listeners.
+  setListeners();
 };
 
-const dom = new Map();
-const allIdElements = document.querySelectorAll(`[id]`);
-for (const element of allIdElements) dom.set(element.id, element);
+const anchorInturupt = (event) => {
+  event.preventDefault();
+  const target = event.target;
 
-dom.get("hamburger").addEventListener("click", handleHamburgerClick);
-dom.get("close-navigation").addEventListener("click", handleCloseNavClick);
-dom.get("navigation-curtain").addEventListener("click", handleCloseNavClick);
+  renderMainContent(target.href);
+};
 
-ultimateNonBreakingSpaceInsert();
+const renderMainContent = async (href) => {
+  const requestHTMLDocument = await fetch(href);
+  const htmlText = await requestHTMLDocument.text();
+
+  const shadowRoot = document.createElement("html");
+  shadowRoot.innerHTML = htmlText;
+
+  const shadowMain = shadowRoot.querySelector("#main-grid");
+  dom.get("main-grid").innerHTML = shadowMain.innerHTML;
+
+  handleCloseNavClick();
+};
+
+const assignAnchorListeners = () => {
+  const allAnchorElements = document.querySelectorAll(`a:not(a[target="_blank"])`);
+  for (const element of allAnchorElements) element.addEventListener("click", anchorInturupt);
+};
+
+const setListeners = () => {
+  dom.get("hamburger").addEventListener("click", handleHamburgerClick);
+  dom.get("close-navigation").addEventListener("click", handleCloseNavClick);
+  dom.get("navigation-curtain").addEventListener("click", handleCloseNavClick);
+};
+
+const observer = new MutationObserver(runDomMapThrottle);
+observer.observe(document.body, { attributes: false, childList: true, subtree: true });
+
+importComponents();
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Read Me
+
+## To Do
+
+- Build dom map into function. Run on initial load and <main> render.
